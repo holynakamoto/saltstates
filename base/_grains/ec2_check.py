cd91942aa8e32bc6cf0bfa9e18fd082cffb5a404
Mo Ali <akali1987@gmail.com>
2017-04-29 18:39:23 -0400

    Remove multiple lists and use single lise in custom grain

--- base/_grains/ec2_check.py
+++ base/_grains/ec2_check.py
@@ -21,9 +21,9 @@ def ec2_check():
 
         # Set grains provider to Amazon
         grains['cloud_info'] = []
-        grains['cloud_info'].append({'provider': 'Amazon'})
-        grains['cloud_info'].append({'instance_type': instance_type})
-        grains['cloud_info'].append({'instnace_region': instance_region})
+        grains['cloud_info'][0] = {'provider': 'Amazon'}
+        grains['cloud_info'][0] = {'instance_type': instance_type}
+        grains['cloud_info'][0] = {'instnace_region': instance_region}
 
         return grains
     except HTTPError:
