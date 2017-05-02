f88a21b3804d56046571c5cffac560f7d8f960f8
Mo Ali <akali1987@gmail.com>
2017-05-01 22:42:44 -0400

    ec2_check custom grains

---
+++ base/_grains/ec2_check.py
@@ -0,0 +1,25 @@
+import urllib2
+
+def ec2_check():
+
+    # Instantiate grains dictionary
+    grains = {}
+
+    # Instiate grains key cloud info
+    grains['cloud_info'] = []
+
+    # Base url
+    base_url = 'http://169.254.169.254/latest/meta-data/'
+
+    instance_id = urllib2.urlopen(base_url + '/instance-id')
+    instance_type = urllib2.urlopen(base_url + 'instance-type')
+    instance_id = instance_id.read()
+    instance_type = instance_type.read()
+
+    grains['cloud_info'].append({'providier': 'Amazon'})
+    grains['cloud_info'][0]['instance_id'] = instance_id
+    grains['cloud_info'][0]['instance_type'] = instance_type
+    return grains
+
+if __name__ == '__main__':
+    ec2_check()
