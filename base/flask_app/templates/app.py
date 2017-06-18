90b29459c2319260b80043395c6d637e17d0715d
Mo Ali <akali1987@gmail.com>
2017-06-17 21:12:16 -0400

    get web01 mined ip

--- base/flask_app/templates/app.py
+++ base/flask_app/templates/app.py
@@ -4,11 +4,11 @@ from flask import Flask
 __version__ = '0.2'
 
 app = Flask(__name__)
-caller = salt.client.Caller()
-mined_ips = caller.cmd('mine.get', 'web01', 'network.ip_addrs')
+mined_ips = salt.client.Caller()
+web01_ip = mined_ips.cmd('mine.get', 'web01', 'network.ip_addrs')
 
 @app.route('/')
 def index():
-    return 'Hello from salt, this is the web01 IP {0}'.format(mined_ips['web01'])
+    return 'Hello from salt, this is the ip from web01 {0}'.format(web01_ip['web01'][0])
 
 app.run(host='0.0.0.0', port='8000')
